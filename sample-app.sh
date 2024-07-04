3a6e71f3ccb523071d17c021bc83ec59da7e8657
GROSH25 <re.appelgren@duocuc.cl>
2024-07-04 19:41:35 +0000

    Docker

--- sample-app.sh
+++ sample-app.sh
@@ -13,10 +13,10 @@ echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "EXPOSE 8500" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
-docker run -t -d -p 5050:5050 --name Trabajo-CCNA sampleapp
-docker ps -a 
+docker run -t -d -p 8500:8500 --name Trabajo-DEVNET sampleapp
+docker ps -a
